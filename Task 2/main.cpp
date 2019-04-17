f32438b6726812816fb66d50fcffd2f2bbd2e563
plskstr <misyura04@gmail.com>
2019-04-17 20:09:49 +0300

    Add prototype of functions and shablon for main

---
+++ Task 2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+template <typename T1, typename T2>
+auto Diff(T1 number1, T2 number2);
+template <typename T1, typename T2, typename T3>
+auto Max(T1 number1, T2 number2, T3 number3);
+
+int main()
+{
+	system("pause");
+	return 0;
+}
